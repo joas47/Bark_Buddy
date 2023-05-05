d54bed33a364b6bff436c53253a3578276bcbfb5
Jonathan <joas4713@student.dsv.se>
2023-05-05 22:43:17 +0200

    form validation on create owner profile

--- lib/make_owner_profile_page.dart
+++ lib/make_owner_profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:cross_platform_test/make_dog_profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'file_selector_handler.dart';
 
@@ -20,10 +21,8 @@ class _MakeOwnerProfilePageState extends State<MakeOwnerProfilePage> {
 
   final List<String> _genderOptions = ['Man', 'Kvinna', 'Annan'];
 
-  final _fNameController = TextEditingController();
-  final _lNameController = TextEditingController();
-  final _ageController = TextEditingController();
-  final _bioController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
 
   @override
   Widget build(BuildContext context) {
@@ -37,30 +36,14 @@ class _MakeOwnerProfilePageState extends State<MakeOwnerProfilePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 64.0),
               const SizedBox(height: 16.0),
-              TextField(
-                controller: _fNameController,
-                decoration: const InputDecoration(
-                  labelText: 'Namn',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.name,
-                onChanged: (value) {
-                  _fName = value;
-                },
+              Form(
+                key: _formKey,
+                autovalidateMode: _autoValidate
+                    ? AutovalidateMode.onUserInteraction
+                    : AutovalidateMode.disabled,
+                child: _formUI(),
               ),
-              const SizedBox(height: 16.0),
-              TextField(
-                  controller: _lNameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Efternamn',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.name,
-                  onChanged: (value) {
-                    _lName = value;
-                  }),
               const SizedBox(height: 16.0),
               // TODO: move this Text so it's next to the radio buttons instead of above.
               const Text('Kön'),
@@ -68,63 +51,30 @@ class _MakeOwnerProfilePageState extends State<MakeOwnerProfilePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: _genderOptions
                     .map((option) => Row(
-                  children: [
-                    Radio(
-                      value: option,
-                      groupValue: _gender,
-                      onChanged: (value) {
-                        setState(() {
-                          _gender = value.toString();
-                        });
-                      },
-                    ),
-                    Text(option),
-                    const SizedBox(width: 16.0),
-                  ],
-                ))
+                          children: [
+                            Radio(
+                              value: option,
+                              groupValue: _gender,
+                              onChanged: (value) {
+                                setState(() {
+                                  _gender = value.toString();
+                                });
+                              },
+                            ),
+                            Text(option),
+                            const SizedBox(width: 16.0),
+                          ],
+                        ))
                     .toList(),
               ),
               const SizedBox(height: 16.0),
-              TextField(
-                controller: _ageController,
-                decoration: const InputDecoration(
-                  labelText: 'Ålder',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  _age = int.tryParse(value) ?? -1;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextField(
-                controller: _bioController,
-                keyboardType: TextInputType.multiline,
-                minLines: 4,
-                maxLines: null,
-                decoration: const InputDecoration(
-                  labelText: 'Om dig',
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (value) {
-                  _bio = value;
-                },
-              ),
               _buildProfilePictureUploadButton(),
               const SizedBox(height: 16.0),
               Builder(builder: (BuildContext context) {
                 return ElevatedButton(
                   onPressed: () {
-                    // TODO: divide into multiple checks, give more specific error messages
-                    // TODO: remove the true in the if statement, only for testing
-                    if (/*_fName.isNotEmpty &&
-                        _lName.isNotEmpty &&
-                        _gender.isNotEmpty &&
-                        !_age.isNegative &&
-                        !_age.isNaN &&
-                        _bio.isNotEmpty &&
-                        _profilePic != null*/
-                    true) {
+                    // TODO: uncomment this
+                    if (/*_validateInputs() && _gender.isNotEmpty && _profilePic != null*/ true) {
                       // TODO: save owner to database (uncomment the line below)
                       //DatabaseHandler.addUserToDatabase(_fName, _lName, _gender, _age, _bio, _profilePic!);
                       Navigator.push(
@@ -148,6 +98,93 @@ class _MakeOwnerProfilePageState extends State<MakeOwnerProfilePage> {
         ),
       ),
     );
+  }
+
+  Widget _formUI() {
+    return Column(children: [
+      TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your Förnamn.';
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Förnamn',
+          border: OutlineInputBorder(),
+        ),
+        keyboardType: TextInputType.name,
+        onChanged: (value) {
+          _fName = value;
+        },
+      ),
+      const SizedBox(height: 16.0),
+      TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your Efternamn.';
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Efternamn',
+          border: OutlineInputBorder(),
+        ),
+        keyboardType: TextInputType.name,
+        onChanged: (value) {
+          _lName = value;
+        },
+      ),
+      const SizedBox(height: 16.0),
+      TextFormField(
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your age.';
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Ålder',
+          border: OutlineInputBorder(),
+        ),
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          _age = int.tryParse(value) ?? -1;
+        },
+      ),
+      const SizedBox(height: 16.0),
+      TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your bio.';
+          }
+          return null;
+        },
+        keyboardType: TextInputType.multiline,
+        minLines: 4,
+        maxLines: 8,
+        decoration: const InputDecoration(
+          labelText: 'Om dig',
+          border: OutlineInputBorder(),
+        ),
+        onChanged: (value) {
+          _bio = value;
+        },
+      ),
+    ]);
+  }
+
+  bool _validateInputs() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      return true;
+    } else {
+      setState(() {
+        _autoValidate = true;
+      });
+      return false;
+    }
   }
 
   Widget _buildProfilePictureUploadButton() {
