db216ae5db14a66aef39724692d97db7bd7e9626
sayedkaraeen <jkaraeen@gmail.com>
2023-05-08 11:45:21 +0200

    //should see this in the login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:cross_platform_test/login_page.dart';
 import 'package:flutter/material.dart';
-
+//should see this
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
