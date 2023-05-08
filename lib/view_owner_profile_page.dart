15a24636118ead1f76632b4301808e663b2cdf05
missTangerine <moa.wieweg@outlook.com>
2023-05-08 17:08:15 +0200

    view_owner_profile hämtar nu från databas

--- lib/view_owner_profile_page.dart
+++ lib/view_owner_profile_page.dart
@@ -1,20 +1,22 @@
-import 'package:cross_platform_test/settings_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'settings_page.dart';
 
 class ViewOwnerProfile extends StatelessWidget {
-  const ViewOwnerProfile({super.key});
+  const ViewOwnerProfile({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    final userUid = FirebaseAuth.instance.currentUser?.uid;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ägarprofil'),
+        title: const Text('Ownerprofile'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // TODO: Implement settings functionality.
-              // goto settings page
               Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -23,93 +25,111 @@ class ViewOwnerProfile extends StatelessWidget {
           ),
         ],
       ),
-      body: Stack(alignment: Alignment.center, children: <Widget>[
-        Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: ElevatedButton(
-                  child: const Text('Lägg till plats'),
-                  onPressed: () {},
-                ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: const CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage:
-                    // TODO: get this information from the database
-                    AssetImage('assets/images/placeholder-dog-image2.png'),
+      body: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(userUid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            }
+
+            final userData = snapshot.data!;
+            final name = userData.get('name');
+            final surname = userData.get('surname');
+            final about = userData.get('about') as String?;
+            final age = userData.get('age') as int?;
+            final gender = userData.get('gender');
+            final String? profilePic = userData.get('picture') as String?;
+
+            return Stack(alignment: Alignment.center, children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: ElevatedButton(
+                        child: const Text('Lägg till plats'),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage: AssetImage(
+                              'assets/images/placeholder-dog-image2.png'),
+                        ),
+                      ),
+                    ),
+                  ]),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 100.0,
+                    backgroundImage: profilePic != null
+                        ? NetworkImage(profilePic)
+                        : AssetImage(
+                        'assets/images/placeholder-profile-image.png') as ImageProvider<Object>,
                   ),
-                ),
+                  Text(
+                    name + ' ' + surname,
+                    style: const TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    //height: 500.0,
+                    width: 300.0,
+                    child: TextField(
+                      readOnly: true,
+                      minLines: 1,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        // TODO: get this information from the database
+                          hintText:
+                          '• ' + gender! + '\n• ' + age.toString(),
+                          border: const OutlineInputBorder(),
+                          suffixIcon: IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {
+                              //Navigator.push(context, MaterialPageRoute(builder: (context) => const EditOwnerProfile()));
+                            },
+                          )),
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  SizedBox(
+                    //height: 500.0,
+                    width: 300.0,
+                    child: TextField(
+                      readOnly: true,
+                      minLines: 5,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        // TODO: get this information from the database
+                        hintText: '• ' + about!,
+                        border: OutlineInputBorder(),
+                      ),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ]),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(height: 10.0),
-            const CircleAvatar(
-              radius: 100.0,
-              backgroundImage:
-              // TODO: get this information from the database
-              AssetImage('assets/images/placeholder-profile-image.png'),
-            ),
-            const Text(
-              'John',
-              style: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              //height: 500.0,
-              width: 300.0,
-              child: TextField(
-                readOnly: true,
-                minLines: 1,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  // TODO: get this information from the database
-                    hintText:
-                    '• Man \n• Stor \n• Golden Retriever lover\n• Hög aktivitetsnivå',
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () {
-                        //Navigator.push(context, MaterialPageRoute(builder: (context) => const EditOwnerProfile()));
-                      },
-                    )),
-                style: const TextStyle(
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            const SizedBox(
-              //height: 500.0,
-              width: 300.0,
-              child: TextField(
-                readOnly: true,
-                minLines: 5,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  // TODO: get this information from the database
-                  hintText: '• Placeholder bio',
-                  border: OutlineInputBorder(),
-                ),
-                style: TextStyle(
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ]),
+            ]);
+          }),
     );
   }
 }
