ea432b3e7b75faa7b286df76bf96537c3a580a8c
Jonathan <joas4713@student.dsv.se>
2023-05-19 09:07:37 +0200

    commented out check if user has no friend

--- lib/view_dog_profile_page.dart
+++ lib/view_dog_profile_page.dart
@@ -59,11 +59,12 @@ class _ViewDogProfilePageState extends State<ViewDogProfilePage> {
           if (!snapshot.data!.exists) {
             return const Text('This owner has no dog yet.');
           }
-          try {
+          // TODO: this breaks view_dog_profile_page.dart
+/*          try {
             snapshot.data!.get('friends');
           } on StateError {
             return const Text('StateError: This owner has no friends yet.');
-          }
+          }*/
 
           final dogData = snapshot.data!;
           final activityLevel = dogData.get('Activity Level');
