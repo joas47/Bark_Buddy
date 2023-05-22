8ad8dcd3818e0bd4ccabab745ca523461ae93ca4
Jonathan <joas4713@student.dsv.se>
2023-05-22 12:50:33 +0200

    now displays all users with dogs except current user

--- lib/find_match_page.dart
+++ lib/find_match_page.dart
@@ -27,106 +27,108 @@ class _FindMatchPageState extends State<FindMatchPage> {
           child: StreamBuilder<QuerySnapshot>(
             stream: FirebaseFirestore.instance
                 .collection('users')
-                .where(FieldPath.documentId,
-                isNotEqualTo: FirebaseAuth.instance.currentUser!.uid)
                 .snapshots(),
             builder: (context, userSnapshot) {
               if (userSnapshot.hasData) {
-                return CarouselSlider.builder(
-                  itemCount: userSnapshot.data!.docs.length,
-                  itemBuilder: (context, int itemIndex, int pageViewIndex) {
-                    DocumentSnapshot doc = userSnapshot.data!.docs[itemIndex];
-                    //String ownerRef = doc.get('owner');
-                    //print(ownerRef);
+            final userDocs = userSnapshot.data!.docs;
+            List<QueryDocumentSnapshot> toRemove = [];
+            for (var doc in userDocs) {
+              // TODO: should not include user matches or pending likes
+              doc.id == FirebaseAuth.instance.currentUser!.uid
+                  ? toRemove.add(doc)
+                  : null;
+              doc.data().toString().contains('dogs') ? null : toRemove.add(doc);
+              //doc.data().toString().contains('matches') ? toRemove.add(doc) : null;
+            }
+            userDocs.removeWhere((element) => toRemove.contains(element));
+            return CarouselSlider.builder(
+              itemCount: userDocs.length,
+              itemBuilder: (context, int itemIndex, int pageViewIndex) {
+                DocumentSnapshot doc = userDocs[itemIndex];
 
-                    if (doc.get('dogs') == null) {
-                      // DO SOMETHING HERE when yourField doesn't exist
+                return StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Dogs')
+                      .doc(doc['dogs'])
+                      .snapshots(),
+                  builder: (context, dogSnapshot) {
+                    if (dogSnapshot.hasError) {
+                      return const Text(
+                          'Something went wrong: user has no dog');
                     }
-
-                    return StreamBuilder<DocumentSnapshot>(
-                      // TODO: should not include user matches or pending likes
-                      stream: FirebaseFirestore.instance
-                          .collection('Dogs')
-                          .doc(doc['dogs'])
-                          .snapshots(),
-                      builder: (context, dogSnapshot) {
-                        if (dogSnapshot.hasError) {
-                          return const Text(
-                              'Something went wrong: user has no dog');
-                        }
-                        if (dogSnapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Text("Loading");
-                        }
-                        final dogDoc = dogSnapshot.data!;
-                        List<dynamic>? dogPicURLs = dogDoc['pictureUrls'];
-                        if (dogPicURLs != null && dogPicURLs.isEmpty) {
-                          return const Text('Error: dog has no picture');
-                        }
-                        String dogPicURL = dogDoc['pictureUrls'][0];
-                        return Column(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          ViewOwnerProfile(userId: doc.id)),
-                                );
-                              },
-                              child: Image(
-                                  image: NetworkImage(doc['picture']), height: 100),
-                            ),
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          ViewDogProfilePage(userId: doc.id)),
-                                );
-                              },
-                              child: Container(
-                                height: 300,
-                                margin: const EdgeInsets.all(6.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  image: DecorationImage(
-                                    image: NetworkImage(dogPicURL),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
+                    if (dogSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Text("Loading");
+                    }
+                    final dogDoc = dogSnapshot.data!;
+                    List<dynamic>? dogPicURLs = dogDoc['pictureUrls'];
+                    if (dogPicURLs != null && dogPicURLs.isEmpty) {
+                      return const Text('Error: dog has no picture');
+                    }
+                    String dogPicURL = dogDoc['pictureUrls'][0];
+                    return Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ViewOwnerProfile(userId: doc.id)),
+                            );
+                          },
+                          child: Image(
+                              image: NetworkImage(doc['picture']), height: 100),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ViewDogProfilePage(userId: doc.id)),
+                            );
+                          },
+                          child: Container(
+                            height: 300,
+                            margin: const EdgeInsets.all(6.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              image: DecorationImage(
+                                image: NetworkImage(dogPicURL),
+                                fit: BoxFit.cover,
                               ),
                             ),
-                            Row(
-                              children: [
-                                Text(dogDoc['Name'] +
-                                    ", " +
-                                    dogDoc['Age'].toString() +
-                                    " " +
-                                    dogDoc['Gender']),
-                                IconButton(
-                                  // TODO: make this a heart icon
-                                  icon: const Icon(Icons.heart_broken),
-                                  onPressed: () {
-                                    // TODO send like
-                                    DatabaseHandler.sendLike(doc.id);
-                                  },
-                                ),
-                              ],
-                            )
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(dogDoc['Name'] +
+                                ", " +
+                                dogDoc['Age'].toString() +
+                                " " +
+                                dogDoc['Gender']),
+                            IconButton(
+                              // TODO: make this a heart icon
+                              icon: const Icon(Icons.heart_broken),
+                              onPressed: () {
+                                // TODO: should work, test this some more
+                                DatabaseHandler.sendLike(doc.id);
+                              },
+                            ),
                           ],
-                        );
-                      },
+                        )
+                      ],
+                    );
+                  },
                     );
                   },
                   // TODO: this should take into account the size of the screen and try to fill as much as possible
                   options: CarouselOptions(height: 600),
                 );
               } else {
-                return const Text("No data");
-              }
+                return const Text("Snapshot has no data");
+          }
             },
           )),
       /*body: SingleChildScrollView(
