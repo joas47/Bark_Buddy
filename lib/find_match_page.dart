aec594b4c0b09af8e8ee92de03f2a1d3935b738b
Jonathan <joas4713@student.dsv.se>
2023-05-25 11:31:16 +0200

    now checks if the current user's availability is set for the current day before showing potential matches

--- lib/find_match_page.dart
+++ lib/find_match_page.dart
@@ -456,10 +456,7 @@ class _FindMatchPageState extends State<FindMatchPage> {
                   // remove the current user from the list of potential matches (shouldn't match with yourself)
                   userDocs.remove(currentUserDoc);
                   // until the user has set their availability, they shouldn't be able to see any matches
-                  if (currentUserDoc
-                      .data()
-                      .toString()
-                      .contains('availability')) {
+                  if (_isAvailabilityValid(currentUserDoc)) {
                     Set<DocumentSnapshot<Object?>> toRemove =
                         _filterOutUsers(userDocs, currentUserDoc);
                     userDocs
@@ -475,8 +472,8 @@ class _FindMatchPageState extends State<FindMatchPage> {
                   } else {
                     // TODO: make this message prettier
                     return const Center(
-                      child:
-                          Text('Click the clock icon to set your availability'),
+                      child: Text(
+                          'Click the clock icon to set your availability for today!'),
                     );
                   }
                   // TODO: make this message prettier
@@ -851,6 +848,22 @@ class _FindMatchPageState extends State<FindMatchPage> {
         );
       }).toList(),
     );
+  }
+
+  bool _isAvailabilityValid(DocumentSnapshot<Object?> userDoc) {
+    // if the user has not set their availability yet, return false
+    if (userDoc.data().toString().contains('availability')) {
+      Timestamp availability = userDoc['availability']['createdOn'];
+      DateTime dateTime = availability.toDate();
+
+      // if the availability is from yesterday, it's not valid, return false
+      if (DateUtils.isSameDay(dateTime, DateTime.now())) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
   }
 
   /*@override
