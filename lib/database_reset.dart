38523f36631ff8e4734c264fe15f5db76e471fe2
Jonathan <joas4713@student.dsv.se>
2023-05-26 03:01:46 +0200

    created methods to reset the database for testing purposes

---
+++ lib/database_reset.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseReset {
+  Future<void> resetEssentialFieldsInUserCollection() async {
+    // Get a reference to the collection
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection('users');
+
+    // Get all documents in the collection
+    QuerySnapshot querySnapshot = await collectionRef.get();
+
+    // Iterate through each document
+    querySnapshot.docs.forEach((DocumentSnapshot docSnapshot) async {
+      // Reset all fields in the document
+      // commented out so we don't accidentally break something
+/*      await collectionRef.doc(docSnapshot.id).set({
+        //'LastLocation': null, this fucks with too much to reset
+        'availability': [],
+        'pendingLikes': [],
+        'friends': [],
+        'friendrequests': [],
+        'receivedLikes': [],
+        'matches': [],
+      }, SetOptions(merge: true));*/
+    });
+  }
+
+  Future<void> resetEssentialFieldsInUserDocument(String documentID) async {
+    // Get a reference to the document
+    DocumentReference documentRef =
+        FirebaseFirestore.instance.collection('users').doc(documentID);
+
+    // Reset all fields in the document
+    await documentRef.set({
+      //'LastLocation': null, this fucks with too much to reset
+      'availability': [],
+      'pendingLikes': [],
+      'friends': [],
+      'friendrequests': [],
+      'receivedLikes': [],
+      'matches': [],
+    }, SetOptions(merge: true));
+  }
+}
